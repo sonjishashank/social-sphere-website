9f6662907720e61bb0dc69adee82ca7e5699ad70
starsetsolutions-prog <starsetsolutions@gmail.com>
2026-09-09 00:35:39 +0530

    cloudflare

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,20 @@
 import path from 'path';
 import react from '@vitejs/plugin-react';
+import { cloudflare } from '@cloudflare/vite-plugin';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    cloudflare(),
+  ],
+
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
   },
+
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
